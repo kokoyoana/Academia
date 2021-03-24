8e182602f9c7df73ef9e972add1c7126b502293c
yoana <koko-yoana@hotmail.es>
2021-03-24 16:39:42 +0100

    conexion apy

--- academia/app/static/js/script.js
+++ academia/app/static/js/script.js
@@ -1,2 +1,58 @@
-console.log('hola');
+$(document).ready(llamadaApi);
+
+
+
+    function llamadaApi(){
+    $.ajax({
+
+        url: 'https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=c1dkaen48v6tbf1bn6a0',
+        type: 'get',
+        dataType: 'json',
+        headers: { 
+            //'Access-Control-Allow-Origin':'*',
+            // 'X-Finnhub-Token' : 'sandbox_c1dk6ff48v6tbf1bn360'
+        
+        }, 
+
+        success: function(data) {
+            console.log(data)
+
+
+        },
+    });
+}
+
+
+
+
+
+
+
+
+/* 
+    fetch(url,{
+        method: 'get',
+        headers: {
+            'Content-Type' : 'application/json' ,   
+            'Access-Control-Allow-Origin':'*',  
+        }
+    })
+    .then(function(response){
+
+        console.log(response.data);
+
+    })
+        .catch(function(error) {
+                alert("Invalid user"+ error);
+            });
+
+}
+callApy();
+ */
+
+
+
+
+
+
 
